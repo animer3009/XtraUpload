57c759f27df38ce7cf51d46ceb1f9eb4bed23539
Joel Larson <joellarsonweb@gmail.com>
2013-07-06 17:51:17 -0500

    Refactored Xor_string to be easier to read

--- application/libraries/xor_string.php
+++ application/libraries/xor_string.php
@@ -29,41 +29,44 @@
 
 class Xor_string
 {
-
-	function process($str, $crypt_direction, $key='')
+	public function process($inputString, $encryptionDirection, $key = '')
 	{
-	$crypt_method = ( $crypt_direction == 'encrypt' ? 'encrypt' : 'decrypt' );
-		$new_pass = $this->$crypt_method($str, $key);
-		return $new_pass;
-	}
-	
+        if(!in_array($encryptionDirection, array('encrypt','decrypt'))) {
+            throw new InvalidArgumentException("Supproted encryption directions are encrypt and decrypt");
+        }
 
-	function encrypt($str, $enc_string) 
+        return $this->$encryptionDirection($inputString, $key);
+	}
+
+	public function encrypt($inputString, $enc_string)
 	{	
 		$str_encrypted = "";
+
 		if ($enc_string % 2 == 1) { // we need even number of characters
 			$enc_string .= $enc_string{0};
 		}
-		for ($i=0; $i < strlen($str); $i++) { // encrypts one character - two bytes at once
-			$str_encrypted .= sprintf("%02X", hexdec(substr($enc_string, 2*$i % strlen($enc_string), 2)) ^ ord($str{$i}));
+
+		for ($i=0; $i < strlen($inputString); $i++) { // encrypts one character - two bytes at once
+			$str_encrypted .= sprintf("%02X", hexdec(substr($enc_string, 2*$i % strlen($enc_string), 2)) ^ ord($inputString{$i}));
 		}
 	
 		return $str_encrypted;
-	
-	} 
+	}
 	
 	function decrypt($str_encrypted, $enc_string) 
 	{
 		$str = "";
+
 		if ($enc_string % 2 == 1) 
 		{
 			$enc_string .= $enc_string{0};
 		}
+
 		for ($i=0; $i < strlen($str_encrypted); $i += 2) 
 		{
 			$str .= chr(hexdec(substr($enc_string, $i % strlen($enc_string), 2)) ^ hexdec(substr($str_encrypted, $i, 2)));
 		}
+
 		return $str;
-	
-	} // End function decryptPassword	
+	}
 }
