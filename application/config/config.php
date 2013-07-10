a0fb3d7b3732d11e89119381c28ec9974a6f212c
Matthew Glinski <matthewglinski@gmail.com>
2013-07-10 08:02:24 -0400

    Fixes for new public folder

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/xu/public';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'nonboard';
-$db['default']['database'] = 'xu';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = 'xu2_';
 $db['default']['pconnect'] = TRUE;
