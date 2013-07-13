36c6f5cef675164ba6c5177e8dd92ef1d4f35c24
Matthew Glinski <matthewglinski@gmail.com>
2013-07-13 11:50:24 -0400

    Reverted messed up config files (Do'h!)

--- application/config/database.php
+++ application/config/database.php
@@ -48,14 +48,14 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
+$db['default']['hostname'] = '127.0.0.1';
+$db['default']['username'] = '';
 $db['default']['password'] = '';
-$db['default']['database'] = 'xu2';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = 'xu2_';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
+$db['default']['pconnect'] = FALSE;
+$db['default']['db_debug'] = FALSE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
 $db['default']['char_set'] = 'utf8';
